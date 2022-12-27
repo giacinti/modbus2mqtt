b0d683820bba9d5deebfba443cb9164d854162ec
Philippe Giacinti <philippe@giacinti.com>
2022-12-27 18:44:02 +0100

    modbus_tcp_test removed from last Teltonika firmware - adaptation required

--- modbus2mqtt.sh
+++ modbus2mqtt.sh
@@ -24,7 +24,15 @@ float_div_10 () {
 }
 
 ip_conv () {
-  echo "\"$(echo $1 | sed -e 's/,/\./g')\""
+  echo "\"$(echo $1 | sed -e 's/,/\./g')\"" 
+}
+
+ascii_conv () {
+  echo -e $1 | sed -e 's/\\u0000//g' -e 's/\\"//g'
+}
+
+operator_conv () {
+  echo "\"$(echo $1 | sed -e 's/\"//' | awk '{print $1}')\""
 }
 
 read_modbus () {
@@ -33,17 +41,22 @@ read_modbus () {
   type=$3
   conv=""
   if [ "${type}" = "ascii" ]; then
-     conv="echo -e"
+     conv="ascii_conv"
   elif [ "${type}" = "temp" ]; then
     type="32bit_int1234"
     conv="float_div_10"
   elif [ "${type}" = "ip" ]; then
     type="8bit_uint"
     conv="ip_conv"
+  elif [ "${type}" = "operator" ]; then
+    type="ascii"
+    conv="operator_conv"
   fi
-  ret=$(modbus_tcp_test ${MODBUS_SERVER} ${MODBUS_PORT} 1 1 3 ${reg} ${size} ${type} 1)
+  #ret=$(modbus_tcp_test ${MODBUS_SERVER} ${MODBUS_PORT} 1 1 3 ${reg} ${size} ${type} 1)
+  ret=$(ubus call modbus_master tcp.test "{\"ip\":\"${MODBUS_SERVER}\", \"port\":${MODBUS_PORT}, \"id\":1, \"timeout\":1, \"function\":3, \"first_reg\":${reg}, \"reg_count\":\"${size}\", \"data_type\":\"${type}\", \"no_brackets\":1}" | grep '"result"' | sed -e 's/.*"result"://') 
+  ret=$(eval /bin/echo $ret)
   if [ -n "${conv}" ]; then
-    ret=$(${conv} ${ret})
+    ret=$(${conv} "${ret}")
   fi
   echo ${ret}
 }
